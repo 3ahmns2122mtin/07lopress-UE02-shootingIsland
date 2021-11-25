8a5629a1dd5ae864bdb5086b0371929d5a4aabff
lopress <90834282+lopress@users.noreply.github.com>
2021-11-25 10:10:48 +0100

    aktueller Stand in der Klasse / 25.11.2021

--- Assets/MyGame/Scripts/GameManager.cs
+++ Assets/MyGame/Scripts/GameManager.cs
@@ -1,17 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    private const int maxHit = 10;
     public GameObject target;
     public GameObject parentOfTargets;
     public GameObject objCounter;
+    public GameObject wonObj;
+    public GameObject shootSound;
 
     private Text textCounter;
-    public bool won;
-    public int score;
+    private bool won;
+    private int score;
 
 
 
@@ -21,6 +22,7 @@ public class GameManager : MonoBehaviour
         textCounter = objCounter.GetComponent<Text>();
         won = false;
         InvokeRepeating("Spawn", 1f, 2f);
+        wonObj.SetActive(false);
     }
 
 
@@ -46,6 +48,7 @@ public class GameManager : MonoBehaviour
         if(won == true)
         {
             CancelInvoke("Spawn");
+            wonObj.SetActive(true);
         }
         else
         {
@@ -55,6 +58,7 @@ public class GameManager : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             //Debug.Log("Mouse pressed");
+            shootSound.GetComponent<AudioSource>().Play();
         }
     }
 
@@ -65,7 +69,7 @@ public class GameManager : MonoBehaviour
         Debug.Log("increment ... " + score);
         textCounter.text = score.ToString();
 
-        if (score == 10)
+        if (score == maxHit)
         {
             won = true;
         }
--- Assets/MyGame/Scripts/Target.cs
+++ Assets/MyGame/Scripts/Target.cs
@@ -18,5 +18,4 @@ public class Target : MonoBehaviour
         gameManager.IncrementScore();
         Destroy(gameObject);
     }
-
 }
